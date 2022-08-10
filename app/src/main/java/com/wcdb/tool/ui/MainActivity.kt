1573981a3798a632b8e3ddef5be3f731d70d848a
pander <zgy1014@qq.com>
2022-08-10 09:15:16 +0800

    删除添加资源文件

--- app/src/main/java/com/wcdb/tool/ui/MainActivity.kt
+++ app/src/main/java/com/wcdb/tool/ui/MainActivity.kt
@@ -5,7 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tencent.wcdb.database.SQLiteCipherSpec
+import com.tencent.wcdb.room.db.WCDBOpenHelperFactory
 import com.wcdb.tool.R
+import com.wcdb.tool.constant.StringConstant
+import com.wcdb.tool.dao.AppDatabase
 import com.wcdb.tool.dao.DayDBDao
 import com.wcdb.tool.dao.SubFlagModelDao
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,12 +26,39 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
        val view =  LayoutInflater.from(this).inflate(R.layout.activity_main, null)
         setContentView(view)
+        //初始化wcdb数据库
+        initRoom()
     }
 
 
     var dayDBDao: DayDBDao? = null
     var subFlagModelDao: SubFlagModelDao? = null
 //    private val formatterDay = SimpleDateFormat("yyyy-MM-dd")
+private var database: AppDatabase? = null
+
+    private fun initRoom() {
+
+        val cipherSpec = SQLiteCipherSpec()
+            .setPageSize(4096)
+            .setKDFIteration(64000)
+        val factory =
+            WCDBOpenHelperFactory() //  .passphrase("passphrase".getBytes())  // passphrase to the database, remove this line for plain-text
+                .cipherSpec(cipherSpec) // cipher to use, remove for default settings
+                // .writeAheadLoggingEnabled(false)       // enable WAL mode, remove if not needed
+                .asyncCheckpointEnabled(true) // enable asynchronous checkpoint, remove if not needed
+        database = Room.databaseBuilder(
+            context,
+            AppDatabase::class.java, StringConstant.DbBase
+        )
+            .allowMainThreadQueries()
+            .setJournalMode(RoomDatabase.JournalMode.TRUNCATE)
+            .openHelperFactory(factory) // specify WCDBOpenHelperFactory when opening database
+            .build()
+        dayDBDao = database!!.dayDBDao()
+        subFlagModelDao = database!!.subFlagDao()
+
+
+    }
 
 
 
